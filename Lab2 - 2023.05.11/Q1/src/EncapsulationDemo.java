2fac86e8293d81623395ba18fa5fde2af47f776f
Chithraka Kalanamith <chithrakakalanamith@gmail.com>
2023-07-30 19:19:59 +0530

    Done!!

---
+++ Lab2 - 2023.05.11/Q1/src/EncapsulationDemo.java
@@ -0,0 +1,16 @@
+public class EncapsulationDemo {
+    private String empName;
+
+    //Getter and Setter methods
+
+    public String getEmpName(){
+        return empName;
+    }
+
+    public void setEmpName(String newValue){
+        empName = newValue;
+    }
+
+}
+
+
